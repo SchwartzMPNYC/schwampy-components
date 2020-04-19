27be7a30467f1af96527adce51fe318489d5c0ad
Mike Schwartz <schwartzmpnyc@gmail.com>
2020-04-19 17:13:01 -0400

    Add name attribute to demo for testing

--- src/components/Checkbox/Checkbox.demo.js
+++ src/components/Checkbox/Checkbox.demo.js
@@ -1,8 +1,10 @@
 const checkboxStandard = document.createElement("styled-checkbox");
 checkboxStandard.innerText = "Checkbox!?";
+checkboxStandard.setAttribute('name', 'schwampy-checks');
 
 const checkboxCheckedWithAttr = document.createElement("styled-checkbox");
 checkboxCheckedWithAttr.innerText = "I'm checked by default.";
 checkboxCheckedWithAttr.setAttribute("checked", true);
+checkboxCheckedWithAttr.setAttribute('name', 'schwampy-checks');
 
 document.body.append(checkboxStandard, checkboxCheckedWithAttr);
