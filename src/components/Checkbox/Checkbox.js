199ec973119473d362d0c05d6420ed1567b10d5f
Mike Schwartz <schwartzmpnyc@gmail.com>
2020-04-19 16:39:40 -0400

    Using observed attributes, attributeChangedCallback, a getter/setter, and a listener on the child input, allow for changing of checked status with checked attribute on webcomponent. Attribute should stay in sync with checked status of internal checkbox

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -12,6 +12,15 @@ class StyledCheckbox extends HTMLElement {
 		this.attachShadow({ mode: "open" });
 	}
 
+	static get observedAttributes() {
+		return ["checked"];
+	}
+
+	attributeChangedCallback(attr, oldVal, newVal) {
+		if (newVal === null) this[attr] = false;
+		else if (oldVal !== newVal) this[attr] = this.getAttribute(attr);
+	}
+
 	connectedCallback() {
 		const { shadowRoot } = this;
 
@@ -24,6 +33,26 @@ class StyledCheckbox extends HTMLElement {
 		);
 
 		shadowRoot.append(checkboxTemplate);
+		this.input = this.shadowRoot.querySelector("input");
+
+		this.input.addEventListener("change", (event) => {
+			event.target.checked
+				? this.setAttribute("checked", true)
+				: this.removeAttribute("checked");
+		});
+	}
+
+	get checked() {
+		return this.hasAttribute("checked");
+	}
+
+	set checked(checked) {
+		console.log(typeof checked);
+		if (checked) this.setAttribute("checked", checked);
+		else this.removeAttribute("checked");
+
+		this.input.checked =
+			checked && !/(false|null|undefined|0)/.test(checked);
 	}
 }
 
