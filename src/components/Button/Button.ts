2458b38a4ee9b4c8ac310e141cd1bba30793a057
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-03-02 15:46:08 -0500

    Add a `listener` function decorator for applying event listeners to member elements. Tested on button element.

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -1,11 +1,12 @@
-import defineSchwampyComponent from "../../global/decorators/defineSchwampyComponent";
-import SchwampyDefintionConfig from "../../global/interfaces/SchwampyDefintionConfig";
+import defineSchwampyComponent from '../../global/decorators/defineSchwampyComponent';
+import listen from '../../global/decorators/listen';
+import SchwampyDefintionConfig from '../../global/interfaces/SchwampyDefintionConfig';
 
 // @ts-expect-error
-import template from "./Button.template.html";
+import template from './Button.template.html';
 
 const config: SchwampyDefintionConfig = {
-	tagName: "schwampy-btn",
+	tagName: 'schwampy-btn',
 	template,
 };
 
@@ -15,13 +16,19 @@ class Button extends HTMLElement {
 	private counter = 0;
 
 	private connectedCallback() {
-		this.button = this.shadowRoot.querySelector("button");
-		this.button.addEventListener("click", this.clicked);
+		this.button = this.shadowRoot.querySelector('button');
 	}
 
-	public clicked = (): void => {
+	@listen('click', 'button')
+	private increment(event: MouseEvent): void {
 		console.log(`Clicked ${++this.counter} time(s)`);
-	};
+	}
+
+	@listen('mouseout', 'button')
+	@listen('focusout', 'button')
+	private reset(event: MouseEvent): void {
+		this.counter = 0;
+	}
 }
 
 export default Button;
