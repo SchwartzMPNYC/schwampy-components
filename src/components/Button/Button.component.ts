6ddf38454124bf024d6c3625e4037ed0b4cf7391
Mike Schwartz <schwartzmpnyc@gmail.com>
2021-03-12 16:57:02 -0500

    Add a output decorator that can be used to emit a custom event (and optionally a value)

--- src/components/Button/Button.component.ts
+++ src/components/Button/Button.component.ts
@@ -1,5 +1,6 @@
 import { defineSchwampyComponent, SchwampyDefintionConfig } from '../../global/decorators/defineSchwampyComponent';
 import listen from '../../global/decorators/listen';
+import output from '../../global/decorators/output';
 import { attribute, generateAttributeGettersAndSetters, AttributeConfig } from '../../global/decorators/attribute';
 import GetterAndSetter from '../../global/interfaces/GetterAndSetter';
 
@@ -35,13 +36,13 @@ export default class Button extends HTMLElement {
 	}
 
 	@listen('click', (inst: Button) => inst.button)
+	@output('clicked', (inst: Button) => ({ couterValue: inst.counter, loud: inst.loud }))
 	private increment(): void {
 		this.counter++;
 	}
 
 	@listen(['mouseout', 'focusout'], (inst: Button) => inst.button)
-	// @listen('mouseout', (inst: Button) => inst.button)
-	// @listen('focusout', (inst: Button) => inst.button)
+	@output('reset')
 	private reset(): void {
 		this.counter = 0;
 	}
